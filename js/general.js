22358b1ba1d98497b740c67fdf5bcb92622c40d8
L. Antonio G. O <86495681+antoniogzov@users.noreply.github.com>
2022-11-25 11:07:32 -0600

    updates

--- js/general.js
+++ js/general.js
@@ -1,13 +1,34 @@
-$(document).ready(function () {
-   function loading() {
-    Swal.fire({
-      title: "Cargando...",
-      html: '<img src="imgs/loading.gif" width="300" height="175">',
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-      showCloseButton: false,
-      showCancelButton: false,
-      showConfirmButton: false,
-    });
+function timer(ms) {
+  return new Promise((res) => setTimeout(res, ms));
+}
+
+function RQBangFemHebPrim(q) {
+  if (!isNaN(q)) {
+    var q_arr = q.split(".");
+    var q_int = parseInt(q_arr[0]);
+    var q_dec = parseInt(q_arr[1]);
+    if (q == 10) {
+      q_int = 10;
+    } else if (q_dec >= 6) {
+      q_int = q_int + 1;
     }
-});
+    return q_int;
+  } else {
+    return q;
+  }
+}
+
+function redondear(num) {
+  var red = num.split(".");
+  var final = parseInt(red[0]);
+  if (red[1] != "undefined") {
+    if (red[1] > "5") {
+      final = final + 1;
+    }
+  }
+  return final;
+}
+
+function reverse(s) {
+  return s.split("").reverse().join("");
+}
